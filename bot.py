d94201d92751f089476be4a4218a8a649c7363bd
Ihor Paprotskyi <igorpapr@gmail.com>
2020-12-10 02:37:04 +0200

    changed for one bot playing only, added best team_name <3

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import random
 import time
 
 import aiohttp
@@ -10,29 +9,29 @@ import solver
 
 
 class Bot:
-    def __init__(self, loop, rand_sleep):
+    def __init__(self, loop):
         self._api_url = 'http://localhost:8081'
+        self._team_name = 'Дмитрий Гордон'
         self._session = aiohttp.ClientSession()
-        self._players = {}
+        self._player = {}
         self._game = game.Game()
-        self._rand_sleep = rand_sleep
         self._loop = loop
+        self._last_move = {}
 
-    async def _prepare_player(self, num):
+    async def _prepare_player(self):
         async with self._session.post(
                 f'{self._api_url}/game',
-                params={'team_name': num}
+                params={'team_name': self._team_name}
         ) as resp:
             res = (await resp.json())['data']
-            player_num = 1 if res['color'] == 'RED' else 2
-            self._players[player_num] = {
+            self._player = {
                 'color': res['color'],
                 'token': res['token']
             }
 
-    async def _make_move(self, player, move):
+    async def _make_move(self, move):
         json = {'move': move}
-        headers = {'Authorization': f'Token {self._players[player]["token"]}'}
+        headers = {'Authorization': f'Token {self._player["token"]}'}
         async with self._session.post(
                 f'{self._api_url}/move',
                 json=json,
@@ -40,7 +39,7 @@ class Bot:
         ) as resp:
             logging.debug(await resp.text())
             resp = (await resp.json())['data']
-            logging.info(f'Player {player} made move {move}, response: {resp}')
+            logging.info(f'Player {self._team_name} made move {move}, response: {resp}')
 
     async def _get_game(self):
         async with self._session.get(f'{self._api_url}/game') as resp:
@@ -52,25 +51,25 @@ class Bot:
         is_started = current_game_progress['is_started']
 
         while is_started and not is_finished:
+            # waiting until our turn
+            while current_game_progress['whose_turn'] != self._player['color']:
+                current_game_progress = await self._get_game()
+                await asyncio.sleep(0.1)
+
+            # storing last moves of the opponent
+            if current_game_progress['last_move']['player'] != self._player['color']:
+                for move in current_game_progress['last_move']['last_moves']:
+                    self._game.move(move)
+
             player_num_turn = 1 if current_game_progress['whose_turn'] == 'RED' else 2
-            #assert self._game.whose_turn() == player_num_turn
-
-
+            # evaluating time and deciding which move to do
             start = time.time()
-            if player_num_turn == 1:
-                move = random.choice(current_game_progress['possible_moves'])
-            else:
-                move = solver.next_move(self._game, 4, player_num_turn)
+            move = solver.next_move(self._game, 4, player_num_turn)
             end = time.time()
             logging.debug(f'{player_num_turn} {move} {end - start}')
             self._game.move(move)
 
-            await self._make_move(player_num_turn, move)
-
-            if self._rand_sleep:
-                await asyncio.sleep(random.uniform(1.0, 3.5))
-            else:
-                await asyncio.sleep(0.2)
+            await self._make_move(move)
 
             current_game_progress = await self._get_game()
             is_finished = current_game_progress['is_finished']
@@ -83,14 +82,13 @@ class Bot:
         try:
             logging.info('API Tester initialized, test will start in 2 secs')
 
-            asyncio.ensure_future(self._prepare_player(1))
-            asyncio.ensure_future(self._prepare_player(2))
+            await asyncio.ensure_future(self._prepare_player())
 
             logging.info('Game started, players initialized')
 
             await asyncio.sleep(0.5)
 
-            logging.info(f'Players: {self._players}')
+            logging.info(f'Player: {self._player}')
 
             await asyncio.sleep(0.5)
 
